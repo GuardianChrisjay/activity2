ab40cd831fdeb14a8f64a822489b7008885762ba
Chrisjayrowin C. Guardian <145948117+GuardianChrisjay@users.noreply.github.com>
2023-10-10 12:13:01 +0800

    Add files via upload

---
+++ activity2.js
@@ -0,0 +1,26 @@
+let firstFavNum = 22;
+let secondFavNum = 25;
+if (firstFavNum < secondFavNum) {
+console.log(firstFavNum + "is greater than secondFavNum");
+
+} else if (firstFavNum > secondFavNum) {
+console.log(firstFavNum + "is less than secondFavNum");
+}
+else if  (firstFavNum <= secondFavNum) {
+console.log(firstFavNum + "is greater than and equal to secondFavNum");
+}
+else if (firstFavNum <= secondFavNum){
+console.log(firstFavNum + "is less than and equal to secondFavNum");
+}
+else if  (firstFavNum === secondFavNum){
+console.log(firstFavNum + "is  equal to secondFavNum");
+}
+else if (firstFavNum == secondFavNum){
+console.log(firstFavNum + "is equal to secondFavNum");
+}
+else if (firstFavNum !== secondFavNum) {
+console.log(firstFavNum + "is not equal to secondFavNum");
+}
+else if (firstFavNum != secondFavNum) {
+console.log(firstFavNum + "is not equal to secondFavNum");
+}
